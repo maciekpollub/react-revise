aedfc372e2d6b8a8453e86a584e94a04120757ed
Maciej <maciek.ziemba@gmail.com>
2018-05-08 23:19:01 +0200

    easy components with state, props and events...

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './components/Layout'; 
 
 
 ReactDOM.render(<Layout/>, document.getElementById('root'));
-*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Song from './components/Song/Song';
-
-ReactDOM.render(<Song />, document.getElementById('root'));
